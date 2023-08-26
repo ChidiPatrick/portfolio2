327c8de36f446355ddbc4f2f6adfcf82aef75e50
Patrick Chidi <okaforpatrick302@gmail.com>
2023-08-26 14:53:53 +0100

    Implemented email.js functionality

--- src/Components/contact me/contactMe.js
+++ src/Components/contact me/contactMe.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
 function ContactMe() {
+  const form = useRef();
+
+  /// Contact email handler
+  const emailJsHandler = async (e) => {
+    // e.preventDefault();
+
+    console.log("Email handler called");
+
+    emailjs
+      .sendForm(
+        process.env.SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
+        form,
+        process.env.PUBLIC_KEY
+      )
+      .then(
+        (response) => console.log(response),
+        (error) => console.log(`Error: ${error}`)
+      );
+  };
+
   const formik = useFormik({
     initialValues: {
-      name: "",
-      email: "",
+      user_name: "",
+      user_email: "",
     },
     validationSchema: Yup.object({
-      name: Yup.string().required("Required"),
-      email: Yup.string().email("Invalid email address").required("Required"),
+      user_name: Yup.string().required("Required"),
+      user_email: Yup.string()
+        .email("Invalid email address")
+        .required("Required"),
     }),
     onSubmit: (values) => {
       console.log("Submit called");
+      emailJsHandler();
     },
   });
 
@@ -29,7 +54,11 @@ function ContactMe() {
       <h2 className="w-full text-center my-[10px] font-bold text-2xl">
         Contact Me
       </h2>
-      <form onSubmit={formik.handleSubmit} className="w-[90%] my-0 mx-auto">
+      <form
+        ref={form}
+        onSubmit={formik.handleSubmit}
+        className="w-[90%] my-0 mx-auto"
+      >
         <div className="w-full flex justify-between my-4">
           <label htmlFor="name" className="w-[20%] p-1">
             Name:
@@ -39,8 +68,8 @@ function ContactMe() {
             type="text "
             id="name"
             placeholder="Your name"
-            name="name"
-            value={formik.values.name}
+            name="user_name"
+            value={formik.values.user_name}
             onBlur={formik.handleBlur}
             onChange={formik.handleChange}
           />
@@ -56,8 +85,8 @@ function ContactMe() {
             type="email"
             id="userEmail"
             placeholder="your email"
-            name="email"
-            value={formik.values.email}
+            name="user_email"
+            value={formik.values.user_email}
             onBlur={formik.handleBlur}
             onChange={formik.handleChange}
           />
