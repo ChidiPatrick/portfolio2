ee2417225a87941ccf931a9495daa5287d559dcc
Patrick Chidiebele Okafor <okaforpatrick302@gmail.com>
2023-12-22 09:25:10 -0700

    Added some recommendations

--- src/Components/Comments/commentsData.js
+++ src/Components/Comments/commentsData.js
@@ -1,17 +1,31 @@
 const comments = [
   {
     image: "/images/image-0.jpg",
-    fullName: "Debee",
-    title: "Intern at LM Tech hUB",
+    fullName: "Deborah Okeke",
+    title: "Backend Developer",
     recommendation:
       "Being under the supervision of Sir Pat, as I fondly call him, has been very impactful. He possesses an impressive wealth of knowledge and expertise in the field, always staying up-to-date with the latest trends, technologies, and best practices. I strongly admire and I'm inspired by his dedication to continuous learning. His guidance and constructive fedback has helped me to grow as a developer, and his approachable demeanor makes it easy for us to seek guidance whenever we face challenges or need additional clarification I highly recommnd him not only as web developer, but also as a mentor in the field of web development",
   },
   {
     image: "/images/uju.jpg",
-    fullName: "Debee",
+    fullName: "Stephanie Agbo",
     title: "Product designer",
     recommendation:
       "Patrick is a delight to work with. I worked with him on the Attendance management system app. He is a great team lead and an amazing team player. Working with him means being able to be creatively independent while also getting constructive feedback that aligns with the project for an overall success of the project. He is a talented developer that also considers user's experience in development and I think this is quite rare amongst developers. This skill is what I think makes him stand out most as a developer. I highly recommend him",
+  },
+  {
+    image: "/images/Bolowy.jpg",
+    fullName: "Taiwo Bolodeoku",
+    title: "Backend developer",
+    recommendation:
+      "Working with Patrick was a breath of fresh air. His infectious enthusiasm and boundless creativity brought new life to the project as an intern. He challenged me to think outside the box, pushed me to unlearn outdated practices, and ultimately helped me stand out in a crowded marketplace.Patrick is an exceptional developer, hands down. His passion for his craft shines through in every line of code, and his dedication to exceeding expectations is unmatched. I wouldn't hesitate to recommend him to anyone looking for a talented and reliable developer to build their next.",
+  },
+  {
+    image: "/images/Toochi.jpg",
+    fullName: "Toochi Obiefuna",
+    title: "Frontend web developer",
+    recommendation:
+      "Mr Patrick brought invaluable energy and positivity to our tech hub bootcamp. His supervisory skills were exceptional, making the learning process clear, engaging, and even enjoyable. Beyond instruction, he actively collaborated on our projects, providing valuable guidance and boosting team morale. His dedication and positivity would be an asset to any team fortunate enough to have him",
   },
 ];
 
